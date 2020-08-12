c99a689dcd2329fc48b7f41187412e1e3a8069e2
Aqudi <gjdigj@naver.com>
2020-08-12 19:49:30 +0900

    Apply passport

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const passport = require('passport');
+const dotenv = require('dotenv');
+
 const postRouter = require('./routes/post');
 const userRouter = require('./routes/user');
 const db = require('./models');
+const passportConfig = require('./passport');
 
+dotenv.config();
 const app = express();
 
 db.sequelize.sync()
@@ -11,12 +18,21 @@ db.sequelize.sync()
         console.log("db 연결 성공");
     })
     .catch(console.error);
+passportConfig();
 
 app.use(cors({
     origin: '*',
 }))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser(process.env.COOKE_SECRET));
+app.use(session({
+    saveUninitialized: false,
+    resave: false,
+    secret: process.env.COOKE_SECRET,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 app.get("/api", (req, res) => {
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,16 +1,52 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
-const { User } = require('../models/user');
-const { noExtendLeft } = require('sequelize/lib/operators');
+const passport = require('passport');
+
+const { User } = require('../models/user')
+const db = require('../models/index');
 
 const router = express.Router();
 
+router.post("/login", (req, res, next) => {
+    passport.authenticate('local', (err, user, info) => {
+        if (err) {
+            console.error(err);
+            return next(err);
+        }
+        if (info) {
+            return res.status(401).send(info.reason);
+        }
+        return req.login(user, async (loginErr) => {
+            if (loginErr) {
+                console.error(loginErr);
+                return next(loginErr);
+            }
+            const fullUserWithoutPassword = await User.findOne({
+                where: { id: user.id },
+                attributes: {
+                    exclude: ['password'],
+                },
+                include: [{
+                    model: Post,
+                }, {
+                    model: User,
+                    as: 'Followings',
+                }, {
+                    model: User,
+                    as: 'Followers',
+                }]
+            })
+            return res.json(user);
+        });
+    })(req, res, next);
+});
+
 router.post("/", async (req, res, next) => {
+    console.log(req.body);
+
     try {
         const exUser = await User.findOne({
-            where: {
-                email: req.body.email,
-            }
+            where: { email: req.body.email }
         });
         if (exUser) {
             return res.status(403).send('이미 사용중인 아이디입니다.');
@@ -29,8 +65,9 @@ router.post("/", async (req, res, next) => {
     }
 });
 
-router.delete("", (req, res) => {
-    res.json({ id: 1 });
-});
-
+router.post('/logout', (req, res) => {
+    req.logout();
+    req.session.destroy();
+    res.send('ok');
+})
 module.exports = router;
